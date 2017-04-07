3b46b8d0ab2419b80b8bbd2a9796fb6f9747385d
Tim Green <iamtimgreen@gmail.com>
2017-04-07 21:46:30 +1000

    Print usage for 'help'.

--- lib/help.sh
+++ lib/help.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 usage() {
-
   cat << EOF
-help
+usage: hcm <command> [<args>]
+
+Available commands:
+
+   install    Install config module(s) in HOME directory
+   help       Show this doc
+
+See 'hcm help <command>' to read about a specific subcommand.
 EOF
 }
 
