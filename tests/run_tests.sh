1e9130b82eed60687d017dc10ff73aae53da65cf
Tim Green <iamtimgreen@gmail.com>
2017-04-11 20:11:09 +1000

    Add unit tests framework.

---
+++ tests/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+BATS_VERSION=0.4.0
+BATS_PATH=bats/bats-$BATS_VERSION
+[ -d "$BATS_PATH" ] || {
+  wget https://github.com/sstephenson/bats/archive/v${BATS_VERSION}.zip
+  unzip v${BATS_VERSION}.zip -d bats
+  rm -f v${BATS_VERSION}.zip
+}
+
+"$BATS_PATH"/bin/bats tests.bats
